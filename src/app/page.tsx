cb6073991c8a567ff4996d7d546ebcd480ebc426
shah_hoque1 <shah.hoque10@gmail.com>
2024-04-07 12:45:36 +0100

    home cards added and hight change to hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,23 @@
 import React from "react";
 import { ContainerScroll } from "@/app/components/ui/container-scroll-animation";
 import Image from "next/image";
+import { HoverEffect } from "@/app/components/ui/card-hover-effect"
+import { projects } from "@/utils/home/cards";
+
 
 export default function HeroScrollDemo() {
   return (
+
+    <div>
+
     <div className="flex flex-col overflow-hidden">
       <ContainerScroll
         titleComponent={
           <>
             <h1 className="text-4xl font-semibold text-black dark:text-white">
-              AI tools to boost <br />
+              AI tools to boost your <br />
               <span className="text-4xl md:text-[5rem] font-bold mt-1 leading-none">
-                your productivity
+                productivity
               </span>
             </h1>
           </>
@@ -27,6 +33,16 @@ export default function HeroScrollDemo() {
           draggable={false}
         />
       </ContainerScroll>
+    </div>
+
+
+
+<div className="max-w-5xl mx-auto px-8">
+      <HoverEffect items={projects} />
+    </div>
+
+
+
     </div>
   );
 }
---
+++ src/utils/home/cards.ts
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    title: "PDF Chat",
+    description:
+      "Chat directly with your PDF! ",
+    link: "https://stripe.com",
+  },
+  {
+    title: "SQL Chat",
+    description:
+      "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+    link: "https://netflix.com",
+  },
+  {
+    title: "Chat with any website",
+    description:
+      "A multinational technology company that specializes in Internet-related services and products.",
+    link: "https://google.com",
+  },
+];
